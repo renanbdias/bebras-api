e3349a5c4b86fa019573c591e8e01c8e5a351ca9
Jonathan <jonathanheidyjhk@gmail.com>
2018-04-10 16:12:43 -0400

    correção do questions.rb (factory)

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-    factory :competitor do
+    factory :question do
       name { Faker::Name.title }
       difficulty { Faker::Number.between(1, 5) }
       age_group { Faker::Number.between(1, 10) }
