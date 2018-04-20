3c39d6beeea4405ce783aea508b60c0addb0cd1d
Renan Benatti Dias <renanbenattidias@MacBook-Pro-de-Renan.local>
2018-04-19 20:10:00 -0400

    missing did_finish

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,3 +1,3 @@
 class ChallengeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_date, :end_date, :competitor_count, :did_start, :firebase_token, :current_date, :exam_end_date, :duration
+  attributes :id, :name, :start_date, :end_date, :competitor_count, :did_start, :firebase_token, :current_date, :exam_end_date, :duration, :did_finish
 end
