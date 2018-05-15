25e789916d1e91f584f399debb7c270c33cc04a9
Renan Benatti Dias <renanbenattidias@MacBook-Pro-de-Renan.local>
2018-05-15 17:03:47 -0400

    fixing some stuff

--- app/services/get_challenges_service.rb
+++ app/services/get_challenges_service.rb
@@ -3,7 +3,8 @@ class GetChallengesService < BusinessProcess::Base
 
   include ServiceExceptionHandler
 
-  steps :get_challenges
+  steps :get_challenges,
+        :check_expiration
 
   def call
     handle_exception do
@@ -14,10 +15,35 @@ class GetChallengesService < BusinessProcess::Base
   end
 
   private
-    def get_challenges
 
+    # Get all challenges from deputy
+    def get_challenges
       unless @challenges = deputy.challenges
         fail "Desafios não foram encontrados"
+      end
+    end
+
+    # Checks the challenge expiration date (ends challenge if deputy didn't)
+    # Move later to a scope
+    def check_expiration
+      @challenges.each do |challenge|
+        if DateTime.now > (challenge.end_date + ExamDuration.first.exam_duration_in_minutes.minutes)
+          challenge.did_finish = true
+          end_challenge_on_firebase challenge.firebase_token
+        end
+      end
+    end
+
+    # Send a request to firebase to end the challenge
+    def end_challenge_on_firebase firebase_token
+      base_uri = 'https://bebrasbr-6a3ba.firebaseio.com'  # change later to secrets
+
+      firebase = Firebase::Client.new(base_uri)
+
+      @response = firebase.update("challenges/#{@challenge.firebase_token}", { did_finish: true })
+
+      unless @response.success?
+        fail("Something went wrong on firebase")
       end
     end
 end
--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
-# validates :start_date, :deputy, :name, presence: true
-
 RSpec.describe Challenge, type: :model do
   describe 'validations' do
 
-    context 'when challenge_date is nil' do
+    context 'when start_date is nil' do
       subject(:challenge) { build(:challenge, start_date: nil) }
       it 'is invalid if start_date is nil' do
         expect(challenge).to be_invalid
@@ -13,13 +11,37 @@ RSpec.describe Challenge, type: :model do
       end
     end
 
-    # context 'when deputy is nil' do
-    #   subject(:challenge) { build(:challenge, deputy: nil) }
-    #   it 'is invalid if deputy is nil' do
-    #     expect(challenge).to be_invalid
-    #     expect(challenge.errors).to include :deputy
-    #   end
-    # end
+    context "when end_date is nil" do
+      subject(:challenge) { build(:challenge, end_date: nil) }
+      it "is invalid if end_date is nil" do
+        expect(challenge).to be_invalid
+        expect(challenge.errors).to include :end_date
+      end
+    end
+
+    context "when deputy is nil" do
+      subject(:challenge) { build(:challenge, deputy: nil) }
+      it "is invalid if deputy is nil" do
+        expect(challenge).to be_invalid
+        expect(challenge.errors).to include :deputy
+      end
+    end
+
+    context "when name is nil" do
+      subject(:challenge) { build(:challenge, name: nil) }
+      it "is invalid if name is nil" do
+        expect(challenge).to be_invalid
+        expect(challenge.errors).to include :name
+      end
+    end
+
+    context "when challenge did_satrt is false and did_end is true" do
+      subject(:challenge) { build(:challenge, name: nil) }
+      it "is invalid if name is nil" do
+        expect(challenge).to be_invalid
+        expect(challenge.errors).to include :name
+      end
+    end
 
   end
 end
